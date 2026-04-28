07052483fec4e86af9a4d4a52744747b26c84b77
ar00ster18 <arielarusy12@gmail.com>
2026-04-28 11:06:39 +0300

    Fix white/black pawn starting rows in initializePieces

--- ChessGame.java
+++ ChessGame.java
@@ -17,8 +17,8 @@ public class ChessGame {
         // Initialize pieces for both players (white and black)
         // For example, placing pawns
         for (int i = 0; i < 8; i++) {
-            chessBoard.setPiece(i, 1, new Pawn("white"));
-            chessBoard.setPiece(i, 6, new Pawn("black"));
+            chessBoard.setPiece(i, 6, new Pawn("white"));
+            chessBoard.setPiece(i, 1, new Pawn("black"));
         }
         // Initialize other pieces (rooks, knights, bishops, queen, king) similarly
     }
